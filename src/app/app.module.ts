79b088f9152e44af4ffc58a6d625c5fc3a33aaad
Mingyu Gao <mingyu.gao96@gmail.com>
2017-11-11 15:47:33 -0800

    add SlidesPage to declarations and entryComponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { ChartsModule } from 'ng2-charts/ng2-charts';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
 import { HistoryPage } from '../pages/history/history';
+import { HomePage } from '../pages/home/home';
+import { SlidesPage } from '../pages/slides/slides';
+import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,10 +18,10 @@ import { ColorProvider } from '../providers/color/color';
 import { DataProvider } from '../providers/data/data';
 
 @NgModule({
-  declarations: [MyApp, AboutPage, ContactPage, HomePage, TabsPage, HistoryPage, TapColorDirective],
+  declarations: [MyApp, AboutPage, ContactPage, HistoryPage, HomePage, SlidesPage, TabsPage, TapColorDirective],
   imports: [BrowserModule, ChartsModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [MyApp, AboutPage, ContactPage, HomePage, TabsPage, HistoryPage],
+  entryComponents: [MyApp, AboutPage, ContactPage, HistoryPage, HomePage, SlidesPage, TabsPage],
   providers: [
     StatusBar,
     SplashScreen,
