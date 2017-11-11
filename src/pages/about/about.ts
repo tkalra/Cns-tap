b76d7e440b997386977c4f160f9c21c2c6492446
Mingyu Gao <mingyu.gao96@gmail.com>
2017-11-11 14:14:42 -0800

    add slides page

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,35 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SlidesPage } from '../slides/slides';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-  slides = [
-    {
-      title: 'Hello friend',
-      description: "here's some content"
-    },
-    {
-      title: 'This is the second slide',
-      description: 'lorem ipsum...'
-    },
-    {
-      title: 'You can add images',
-      description: "but I don't have one"
-    }
-  ];
-  areSlidesShown: boolean = false;
-
   constructor(public navCtrl: NavController) {}
 
-  toggleSlides() {
-    console.log('here');
-    if (this.areSlidesShown) {
-      this.areSlidesShown = false;
-    } else {
-      this.areSlidesShown = true;
-    }
+  showSlides() {
+    this.navCtrl.push(SlidesPage);
   }
 }
---
+++ src/pages/slides/slides.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-slides',
+  templateUrl: 'slides.html'
+})
+export class SlidesPage {
+  slides = [
+    {
+      title: 'Hello friend',
+      description: "here's some content"
+    },
+    {
+      title: 'This is the second slide',
+      description: 'lorem ipsum...'
+    },
+    {
+      title: 'You can add images',
+      description: "but I don't have one"
+    }
+  ];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+}
