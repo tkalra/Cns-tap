0c0fc1b43cc968827e95334acd17ccce74645ce0
tkalra <tkalraoct31@gmail.com>
2017-11-12 15:42:58 -0800

    disable screen orientation in core platforms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,7 +23,7 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleLightContent();
       splashScreen.hide();
-      if (!platform.is('mobileweb')) {
+      if (!platform.is('core')) {
         screenOrientation.lock(screenOrientation.ORIENTATIONS.PORTRAIT);
       }
     });
