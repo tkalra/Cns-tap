1df23d65460ce61ec1bf006d2b53c6ae6df35314
tkalra <tkalraoct31@gmail.com>
2017-11-17 18:54:31 -0800

    Added an intro to the App

--- src/pages/slides/slides.ts
+++ src/pages/slides/slides.ts
@@ -8,15 +8,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class SlidesPage {
   slides = [
     {
-      title: 'Hello friend',
-      description: "here's some content"
+      title: 'What is the purpose of the app ?',
+      description: `The Finger Tapping Test (FTT; Reitan & Wolfson, 1993),
+                     "also called the Finger Oscillation Test, has a long history in the field of neuropsychology, both as part of the Halstead-Reitan Neuropsychological Battery (HRNB) and as a standalone test 
+                    it can be `
     },
     {
-      title: 'This is the second slide',
+      title: 'How to perform the test ?',
       description: 'lorem ipsum...'
     },
     {
-      title: 'You can add images',
+      title: '',
       description: "but I don't have one"
     }
   ];
