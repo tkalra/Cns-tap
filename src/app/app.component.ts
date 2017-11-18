2d4fa2332ea1b9da1f11411a06508d15f8d41cad
Randy W <randywenas@gmail.com>
2017-11-18 15:10:13 -0800

    Fix tutorial's toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class MyApp {
     platform: Platform,
     statusBar: StatusBar,
     splashScreen: SplashScreen,
-    private screenOrientation: ScreenOrientation
+    screenOrientation: ScreenOrientation
   ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { App } from 'ionic-angular';
 import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 import { HistoryPage } from '../history/history';
 import { AppState, AppStateProvider } from '../../providers/app-state/app-state';
