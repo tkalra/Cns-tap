0414ae41213c37acbfed8d03436c32336b334492
Randy Wenas <randywenas@gmail.com>
2017-11-19 20:30:09 -0800

    Unit testing bitchessssss

--- karma.conf.js
+++ karma.conf.js
@@ -1,70 +1,62 @@
-// Karma configuration
-// Generated on Mon Oct 30 2017 20:39:48 GMT-0700 (PDT)
+var webpackConfig = require('./webpack.test.js');
 
 module.exports = function(config) {
-  config.set({
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+  var _config = {
+    basePath: './',
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
 
-    // list of files / patterns to load in the browser
-    files: ['./src/**/*.spec.ts'],
-
-    // list of files to exclude
-    exclude: [],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: { '/src/**/*.ts': ['typescript'] },
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
-    // web server port
-    port: 9876,
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity,
-
-    typescriptPreprocessor: {
-      // options passed to the typescript compiler
-      options: {
-        sourceMap: false, // (optional) Generates corresponding .map file.
-        target: 'ES5', // (optional) Specify ECMAScript target version: 'ES3' (default), or 'ES5'
-        module: 'commonjs', // (optional) Specify module code generation: 'commonjs' or 'amd'
-        noImplicitAny: false, // (optional) Warn on expressions and declarations with an implied 'any' type.
-        noResolve: false, // (optional) Skip resolution and preprocessing.
-        removeComments: true, // (optional) Do not emit comments to output.
-        concatenateOutput: true // (optional) Concatenate and emit output to single file. By default true if module option is omited, otherwise false.
+    files: [
+      {
+        pattern: './karma-test-shim.js',
+        watched: true
       },
-      // transforming the filenames
-      transformPath: function(path) {
-        return path.replace(/\.ts$/, '.js');
+      {
+        pattern: './src/assets/**/*',
+        watched: false,
+        included: false,
+        served: true,
+        nocache: false
       }
-    }
-  });
+    ],
+
+    proxies: {
+      '/assets/': '/base/src/assets/'
+    },
+
+    preprocessors: {
+      './karma-test-shim.js': ['webpack', 'sourcemap']
+    },
+
+    webpack: webpackConfig,
+
+    webpackMiddleware: {
+      stats: 'errors-only'
+    },
+
+    webpackServer: {
+      noInfo: true
+    },
+
+    browserConsoleLogOptions: {
+      level: 'log',
+      format: '%b %T: %m',
+      terminal: true
+    },
+
+    coverageIstanbulReporter: {
+      reports: ['html', 'lcovonly'],
+      fixWebpackSourcePaths: true
+    },
+
+    reporters: config.coverage ? ['kjhtml', 'dots', 'coverage-istanbul'] : ['kjhtml', 'dots'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadless'],
+    singleRun: false
+  };
+
+  config.set(_config);
 };
--- src/providers/color/color.spec.ts
+++ src/providers/color/color.spec.ts
@@ -1,21 +1,21 @@
 import { Rgb, ColorProvider } from './color';
 
 describe('ColorProvider', () => {
-  var rgb;
-  var colorProvider: ColorProvider;
+  let rgb;
+  let colorProvider: ColorProvider;
 
   beforeEach(() => {
-    rgb = new Rgb();
+    rgb = new Rgb(255, 0, 0);
+    colorProvider = new ColorProvider();
   });
 
   it('should return white when given intensity is 0', () => {
-    Object.assign(rgb, { r: 255, g: 0, b: 0 });
     expect(rgb.r).toBe(255);
     expect(rgb.g).toBe(0);
     expect(rgb.b).toBe(0);
-    let newRgb = colorProvider.getColor(rgb, 0);
+    let newRgb = colorProvider.getColor(rgb, 10);
     expect(newRgb.r).toBe(255);
-    expect(newRgb.g).toBe(255);
-    expect(newRgb.b).toBe(255);
+    expect(newRgb.g).toBe(51);
+    expect(newRgb.b).toBe(51);
   });
 });
